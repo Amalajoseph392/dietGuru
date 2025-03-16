aaff5bf6d6492d8b9d82ff7cad378826708a9307
febinbabu1660 <febinbabu2023a@mca.ajce.in>
2025-03-16 15:27:33 +0530

    Update App.jsx
    Added a signup page

--- User/frontend/src/App.jsx
+++ User/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './assets/components/navbar'
 import LoginPage from './pages/login';
+import SignIn from './pages/signIn';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           {/* Define Routes */}
           <Routes>
             <Route path="/" element={<LoginPage />} /> 
+            <Route path="/signIn" element={<SignIn />} /> 
             {/* <Route path="/home" element={<HomePage />} />  */}
           </Routes>
         </div>
