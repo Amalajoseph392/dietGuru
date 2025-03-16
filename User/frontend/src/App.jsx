90a872d4bcfe39f032d0cb658803fad6eb4ab441
febinbabu1660 <febinbabu2023a@mca.ajce.in>
2025-03-16 14:41:44 +0530

    Update App.jsx
    Added react-route-dom for page routing

--- User/frontend/src/App.jsx
+++ User/frontend/src/App.jsx
@@ -1,11 +1,21 @@
 
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './assets/components/navbar'
+import LoginPage from './pages/login';
 
 function App() {
   return (
     <div>
-<Navbar></Navbar>
+      <Router>
+        <div>
+          {/* Define Routes */}
+          <Routes>
+            <Route path="/" element={<LoginPage />} /> 
+            {/* <Route path="/home" element={<HomePage />} />  */}
+          </Routes>
+        </div>
+      </Router>
     </div>
   )
 }
