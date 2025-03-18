31a30f70f813b2c530c8ec955b21b09a06fdeea7
Amala joseph <amalajoseph392@gmail.com>
2025-03-18 08:38:10 +0530

    home page changes-part1

--- User/frontend/src/pages/about.jsx
+++ User/frontend/src/pages/about.jsx
@@ -16,7 +16,7 @@ function about() {
 
            
             <div className="mt-4 flex flex-col text-center ">
-                <h1 className='text-3xl md:text-4xl whitespace-nowrap font-bold'>Know about <span className='text-emerald-400 hover:text-yellow-400'>Diet Maestro!</span> </h1>
+                <h1 className='text-3xl md:text-4xl whitespace-nowrap font-bold'>Know about <span className='text-yellow-400 hover:text-emerald-400'>Diet Maestro!</span> </h1>
                 <p class="mt-4 text-lg font-semibold px-4 sm:px-8 lg:px-16">At Diet Maestro, we believe that eating well should be easy, enjoyable, and tailored to your lifestyle. Whether you're looking to lose weight, boost energy, or simply enjoy healthier meals, we provide personalized meal plans designed to meet your unique goals.
 
 Our team of nutritionists, chefs, and wellness experts are passionate about creating balanced, delicious, and nutritious meals that fit into your daily routine. We know how challenging it can be to figure out what to eat, so we've done the hard work for you — crafting a variety of meal plans to suit different dietary preferences, restrictions, and tastes.</p>
